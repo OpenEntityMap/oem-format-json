22a128ea8f4aa99b16d153d7d635c53e4ac220d0
Dean Gardiner <me@dgardiner.net>
2016-05-05 16:02:18 +1200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='oem-format-json',
+    version='1.0.0',
+
+    author="Dean Gardiner",
+    author_email="me@dgardiner.net",
+
+    install_requires=[
+        'oem-framework>=1.0.0'
+    ]
+)
