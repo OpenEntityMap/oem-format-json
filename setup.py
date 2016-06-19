6469b84f32a9c98e4f64b85bdc8d965dfb475fbc
Dean Gardiner <me@dgardiner.net>
2016-06-20 01:09:28 +1200

    Updated [setup.py] to use pbr

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@ from setuptools import setup
 
 
 setup(
-    name='oem-format-json',
-    version='1.0.0',
-
-    author="Dean Gardiner",
-    author_email="me@dgardiner.net",
-
-    install_requires=[
-        'oem-framework>=1.0.0'
-    ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
